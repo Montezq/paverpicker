57b2ec64afb90ff8bfeae6dcf9c7d6d73323132e
Monte <intalmaci21@gmail.com>
2022-12-08 21:50:27 +0200

    markup and styling for header and buttons component

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,8 +1,35 @@
 <template>
   <header class="header">
-
+    <div class="container">
+      <div class="row d_flex align-items_center justify-content_center">
+        <div class="header__logo">
+          <NuxtLink to="/">
+            <div v-html="logo"></div>
+          </NuxtLink>
+        </div>
+        <div class="header__menu">
+          <ul class="header__menu-list d_flex">
+            <li class="header__menu-list-item">
+              <NuxtLink to="/" class="text_uppercase fw_semibold">Photography</NuxtLink>
+            </li>
+            <li class="header__menu-list-item">
+              <NuxtLink to="/" class="text_uppercase fw_semibold">Visualisation</NuxtLink>
+            </li>
+            <li class="header__menu-list-item">
+              <NuxtLink to="/" class="text_uppercase fw_semibold">Software</NuxtLink>
+            </li>
+          </ul>
+        </div>
+        <div class="header__cta">
+          <a href="Get started" class="text_uppercase btn btn_primary">Get started</a>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 <style lang="scss">
   @import 'header';
 </style>
+<script setup>
+  import logo  from '~/assets/svg/logo/logo.svg?raw';
+</script>
