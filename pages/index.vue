8dfa625f238ee3794e20519b2376bcd6f5db694b
Monte <intalmaci21@gmail.com>
2022-12-10 18:55:55 +0200

    adding docSlider

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
     <Header />
+    <client-only>
+      <div class="docSlider">
+        <section id="foo">Hello</section>
+        <section id="bar">Hello 2</section>
+        <section id="baz">Hello 3</section>
+      </div>
+    </client-only>
   </div>
 </template>
+<script setup>
+  useHead({
+    script: [
+      { 
+        hid: 'docslider',
+        src: 'https://cdn.jsdelivr.net/npm/docslider@3.0.1/docSlider.min.js',
+        defer: true,
+        async: true,
+        body: true,
+        callback: () => { setTimeout(() => {initDocSlider()}, 1); } 
+      },
+    ],
+  })
+  function initDocSlider(){
+    docSlider.init({
+      pager: false,
+      speed: 1200,
+      easing: 'ease-in-out',
+    })
+  }
+</script>
