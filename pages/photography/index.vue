cffbd36dbf9e736b2087ccafb5d61124f25509b0
Monte <intalmaci21@gmail.com>
2022-12-21 23:45:50 +0200

    rewriting touc event

--- pages/photography/index.vue
+++ pages/photography/index.vue
@@ -161,21 +161,35 @@
     }, speed);
   }
   function touch(){
-    let touchstartY = 0
-    let touchendY = 0
-    function checkDirection() {
-      if (touchendY < touchstartY && ticking.value === false && scrlTicking.value)
-        slide(2200,null,'up')
-      if (touchendY > touchstartY && ticking.value === false && scrlTicking.value)
-        slide(2200,null,'down')
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
+    var xDown = null;                                                        
+    var yDown = null;                                                        
+    function handleTouchStart(evt) {                                         
+      xDown = evt.touches[0].clientX;                                      
+      yDown = evt.touches[0].clientY;                                      
+    };                                                
+    function handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+        return;
+      }
+
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+      if(Math.abs( xDiff )+Math.abs( yDiff )>1){
+        if( Math.abs( yDiff  ) > Math.abs( xDiff ) ) {
+          if( yDiff > 0 && ticking.value === false && scrlTicking.value) 
+            slide(2200,null,'up')
+          if( yDiff < 0 && ticking.value === false && scrlTicking.value)  
+            slide(2200,null,'down')                                                              
+        } 
+        xDown = null;
+        yDown = null;
+      }
     }
-    document.addEventListener('touchstart', e => {
-      touchstartY = e.changedTouches[0].screenY
-    })
-    document.addEventListener('touchend', e => {
-      touchendY = e.changedTouches[0].screenY
-      checkDirection()
-    })
   }
   onMounted(() => {
     setTimeout(() => {
