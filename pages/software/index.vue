8cbb332b125ed56e93ad38eb01a44dbbb4db5354
Monte <intalmaci21@gmail.com>
2022-12-26 18:46:14 +0200

    styling + animations + responsive for software page

--- pages/software/index.vue
+++ pages/software/index.vue
@@ -22,26 +22,88 @@
         </section>
         <section class="software-page__steps">
           <div>
-            <div class="slide-section position_relative d_flex align-items_center">
-              <div class="slide-section__text-wrapper">
-                <div class="slide-section__text position_relative">
-                  <div class="slide-section__text-inside slide-section__text_1 position_relative">
-                    <div class="slide-section__text-head">
-                      <h2>Precise Cut-Outs</h2>
+            <div class="slide-section position_relative ">
+              <div class="d_flex flex_column align-items_center h_100 position_relative">
+                <div class="slide-section__text-wrapper">
+                  <div class="slide-section__text position_relative">
+                    <div class="slide-section__text-inside slide-section__text_1 position_absolute">
+                      <div class="slide-section__text-head text_center">
+                        <h2>Your Online Showroom</h2>
+                      </div>
+                      <div class="slide-section__text-desc text_center">
+                        <p class="fs_32">
+                          Present your products in one place, the
+                          <br> use of filters make browsing easy.
+                        </p>
+                      </div>
                     </div>
-                    <div class="slide-section__text-desc">
-                      <p class="fs_32">
-                        We take no shortcuts when it comes to
-                        <br> preparing the areas to be visualised. We
-                        <br> ensure each and every blade of grass is
-                        <br> properly preserved. 
-                      </p>
+                    <div class="slide-section__text-inside slide-section__text_2 position_absolute">
+                      <div class="slide-section__text-head text_center">
+                        <h2>Limitless Possibilities</h2>
+                      </div>
+                      <div class="slide-section__text-desc text_center">
+                        <p class="fs_32">
+                          Change laying patterns, joint colours and angles.
+                          <br> Take it a step further with the blender module.
+                        </p>
+                      </div>
+                      <div class="slider-section__text-cta text_center">
+                        <NuxtLink to="/" >
+                          <p class="fs_32 fw_regular">
+                            Learn about the blender module
+                          </p>
+                        </NuxtLink>
+                      </div>
+                    </div>
+                    <div class="slide-section__text-inside slide-section__text_3 position_absolute">
+                      <div class="slide-section__text-head text_center">
+                        <h2>Inspiring Visualisation</h2>
+                      </div>
+                      <div class="slide-section__text-desc text_center">
+                        <p class="fs_32">
+                          Check to see how products look by viewing them in a
+                          <br> scene.
+                        </p>
+                      </div>
+                      <div class="slider-section__text-cta text_center">
+                        <NuxtLink to="/" >
+                          <p class="fs_32 fw_regular">
+                            Learn more about visualisation
+                          </p>
+                        </NuxtLink>
+                      </div>
+                    </div>
+                    <div class="slide-section__text-inside slide-section__text_4 position_relative">
+                      <div class="slide-section__text-head text_center">
+                        <h2>Any Device</h2>
+                      </div>
+                      <div class="slide-section__text-desc text_center">
+                        <p class="fs_32">
+                          Fully Adaptable to screen sizes. If you want even more control 
+                          <br> we offer API for integrating straight into your website.
+                        </p>
+                      </div>
+                      <div class="slider-section__text-cta text_center">
+                        <NuxtLink to="/" >
+                          <p class="fs_32 fw_regular">
+                            Learn about the API module
+                          </p>
+                        </NuxtLink>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="slide-section__decor position_relative">
-                
+                <div class="slide-section__decor position_relative">
+                  <div class="slide-section__decor-box slide-section__decor-box_video">
+                    <Img :img="'paver-3'" :alt="'Box'" class="software_paver"/>
+                  </div>
+                </div>
+                <div class="slide-section__decor-box slide-section__decor-box_tablet position_absolute">
+                  <Img :img="'paver-3'" :alt="'Box'" class="software_paver"/>
+                </div>
+                <div class="slide-section__decor-box slide-section__decor-box_mobile position_absolute">
+                  <Img :img="'paver-3mobile'" :alt="'Box'" class="software_paver"/>
+                </div>
               </div>
             </div>
             <div class="scrollable-section">
@@ -65,11 +127,11 @@
       scrlTicking = ref(true);
 
   function nextPage(){
-    slide(2400, 100)
+    slide(3000, 100)
   }
-  function slide(speed=2400, y=null, direction=null) {
+  function slide(speed=3000, y=null, direction=null) {
     const scrl = document.querySelector('.software-page__steps');
-    const lngth = 6;
+    const lngth = 4;
     if((y>0 || direction==='up') && currentSlide.value < lngth){
       currentSlide.value+=1
       pastSlide.value = currentSlide.value-1
@@ -78,10 +140,10 @@
       pastSlide.value = currentSlide.value+1
     }
     ticking.value = true;
-    if(currentSlide.value!==6)
+    if(currentSlide.value!==4)
       scrl.classList.remove('oh')
     setTimeout(() => {
-      if(currentSlide.value===6)
+      if(currentSlide.value===4)
         scrl.classList.add('oh')
       ticking.value = false;
     }, speed);
@@ -108,9 +170,9 @@
       if(Math.abs( xDiff )+Math.abs( yDiff )>1){
         if( Math.abs( yDiff  ) > Math.abs( xDiff ) ) {
           if( yDiff > 0 && ticking.value === false && scrlTicking.value) 
-            slide(2400,null,'up')
+            slide(3000,null,'up')
           if( yDiff < 0 && ticking.value === false && scrlTicking.value)  
-            slide(2400,null,'down')                                                              
+            slide(3000,null,'down')                                                              
         } 
         xDown = null;
         yDown = null;
@@ -129,10 +191,10 @@
       })
       wrapper.addEventListener('wheel', (e) => {
         if (ticking.value === false && scrlTicking.value) {
-          slide(2400, e.deltaY)
+          slide(3000, e.deltaY)
         }
       });
       touch();
-    }, 510);
+    }, 610);
   })
 </script>
