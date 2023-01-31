e086a80cbd69d638a1f9b2c23cdac2c5fc83e8b0
Monte <intalmaci21@gmail.com>
2023-01-31 20:20:06 +0200

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from "vite-svg-loader";
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
