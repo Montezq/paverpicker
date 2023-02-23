913ebec38ae91b3a1f25d7ca61337b5850a116c0
Monte <intalmaci21@gmail.com>
2023-02-23 22:55:29 +0200

    image

--- components/img/index.vue
+++ components/img/index.vue
@@ -9,7 +9,7 @@
       <source v-else type="image/webp" media="(min-width: 1140px)" :srcset="'/images/decor/'+img+'@x2.webp , /images/decor/'+img+'@x2.webp 2x'"/>
       <source v-if="scenes" type="image/webp" media="(max-width: 1139px)" :srcset="'/images/decor/'+img+'-md@x2.webp , /images/decor/'+img+'.webp 2x'"/>
       <source v-else type="image/webp" media="(max-width: 1139px)" :srcset="'/images/decor/'+img+'-md@x2.webp , /images/decor/'+img+'@x2.webp 2x'"/>
-      <img :src="'/images/decor/'+img+'@x2.webp'" :alt="alt" width="500" height="500"/>
+      <img :src="'/images/decor/'+img+'@x2.webp'" :alt="alt" width="500" height="500" loading="lazy"/>
     </picture>
   </div>
 </template>
