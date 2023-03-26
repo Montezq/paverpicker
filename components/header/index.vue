b4a563f8ad74850ba4cd54b30785dc1c0ac37317
Monte <intalmaci21@gmail.com>
2023-03-26 19:10:01 +0300

    markup and styling for the support header buttons

--- components/header/index.vue
+++ components/header/index.vue
@@ -22,9 +22,32 @@
               <NuxtLink to="/software/" :class="['text_uppercase fw_semibold',{'active':isSoftwarePage}]">Software</NuxtLink>
             </li>
           </ul>
+          <div class="d_flex justify-content_center position_absolute header__menu-block">
+            <div class="header__menu-block-item position_relative">
+              <button class="fs_md-10 fs_r12 btn_transparent position_relative">
+                <p class="text_uppercase letter-spacing_1">Support <span class="ff_icon d_ib"> </span></p>
+              </button>
+              <nav class="header__menu-block-item-nav position_absolute text_center">
+                <ul>
+                  <li>
+                    <NuxtLink to="/faq/" class="text_uppercase fs_r12 letter-spacing_1">FAQ</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/scenes/" class="text_uppercase fs_r12 letter-spacing_1">Scenes</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/software/api-module/" class="text_uppercase fs_r12 letter-spacing_1">API Doc</NuxtLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div class="header__menu-block-item">
+              <NuxtLink to="/contacts/" class="text_uppercase fs_md-10 fs_r12 letter-spacing_1">Contact</NuxtLink>
+            </div>
+          </div>
         </nav>
         <div class="header__cta">
-          <NuxtLink to="/contacts/" class="text_uppercase btn btn_primary">Get started</NuxtLink>
+          <NuxtLink to="https://app.paverpicker.com/demo" target="_blank" class="text_uppercase btn btn_primary">Try Demo</NuxtLink>
         </div>
       </div>
     </div>
