f43e0735f55a7c35212bb8b2b27c4805e92b67c8
Monte <intalmaci21@gmail.com>
2023-09-14 12:00:51 +0300

    working with fixes

--- components/img/index.vue
+++ components/img/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="picture">
-    <picture>
+    <img
+      :srcset="
+       '/images/decor/' + img + '.webp 1x, /images/decor/' + img + '@x2.webp 2x'
+      "
+      :sizes="
+        scenes
+          ? '(max-width: 1139px) 100vw, 50vw'
+          : '(min-width: 1140px) 500px, 100vw'
+      "
+      :src="'/images/decor/' + img + '@x2.webp'"
+      :alt="alt"
+      width="500"
+      height="500"
+    />
+
+    <!--<picture>
       <source v-if="resp" type="image/png" media="(min-width: 1140px)" :srcset="'/images/decor/'+img+'.png , /images/decor/'+img+'@x2.png 2x'"/>
       <source v-else type="image/png" media="(min-width: 1140px)" :srcset="'/images/decor/'+img+'@x2.png , /images/decor/'+img+'@x2.png 2x'"/>
       <source v-if="scenes" type="image/png" media="(max-width: 1139px)" :srcset="'/images/decor/'+img+'-md@x2.png , /images/decor/'+img+'.png 2x'"/>
@@ -10,7 +25,7 @@
       <source v-if="scenes" type="image/webp" media="(max-width: 1139px)" :srcset="'/images/decor/'+img+'-md@x2.webp , /images/decor/'+img+'.webp 2x'"/>
       <source v-else type="image/webp" media="(max-width: 1139px)" :srcset="'/images/decor/'+img+'-md@x2.webp , /images/decor/'+img+'@x2.webp 2x'"/>
       <img :src="'/images/decor/'+img+'@x2.webp'" :alt="alt" width="500" height="500"/>
-    </picture>
+    </picture>-->
   </div>
 </template>
 <script setup>
